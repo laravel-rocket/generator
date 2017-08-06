28ad0d223cdc5219239bf2d72dd6f088b70a8099
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2017-08-06 22:39:11 +0800

    :+1: Fixed Alter Table Migration

--- src/Console/Commands/AlterMigrationGeneratorCommand.php
+++ src/Console/Commands/AlterMigrationGeneratorCommand.php
@@ -1,13 +1,13 @@
 <?php
 namespace LaravelRocket\Generator\Console\Commands;
 
-use LaravelRocket\Generator\Generators\CreateMigrationGenerator;
+use LaravelRocket\Generator\Generators\AlterMigrationGenerator;
 
 class AlterMigrationGeneratorCommand extends GeneratorCommand
 {
     protected $name        = 'rocket:make:migration:alter';
 
-    protected $description = 'Create a migration for create table';
+    protected $description = 'Create a migration for alter table';
 
-    protected $generator   = CreateMigrationGenerator::class;
+    protected $generator   = AlterMigrationGenerator::class;
 }
