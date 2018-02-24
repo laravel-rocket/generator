226e2870e5ed56950a1139b76e671fc4f4f9d442
Takaaki Mizuno <takaaki.mizuno@gmail.com>
2018-02-24 23:45:01 +0800

    :bug: Fixed variable name bug

--- stubs/crud/admin/views/show.blade.php
+++ stubs/crud/admin/views/show.blade.php
@@ -39,17 +39,17 @@
                 <td>
 @if( array_key_exists($column['name'], $belongsToRelations) )
 @if( $column['type'] === 'image' )
-＠if( $model->{{ $belongsToRelations[$column['name']]['name'] }} )
+＠if( ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }} )
                 <img src="｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->url ｝｝" class="img-thumbnail" width="50" height="50">
 ＠else
                 <img src="｛｛ \URLHelper::asset('images/no-image.png', 'common') ｝｝" class="img-thumbnail" width="50" height="50">
 ＠endif
 @elseif( $column['type'] === 'file' )
-＠if( $model->{{ $belongsToRelations[$column['name']]['name'] }} )
-                <a href="｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->url ｝｝">｛!! \FileHelper::getFileIconHTML($model->{{ $belongsToRelations[$column['name']]['name'] }}->mime_type) !!｝｛｛ $model->{{ $belongsToRelations[$column['name']]['name'] }}->present()->toString() ｝｝</a>
+＠if( ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }} )
+                <a href="｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->url ｝｝">｛!! \FileHelper::getFileIconHTML(${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->mime_type) !!｝｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->toString() ｝｝</a>
 ＠endif
 @else
-                ｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }} ? $model->{{ $belongsToRelations[$column['name']]['name'] }}->present()->toString() : '' ｝｝
+                ｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }} ? ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->toString() : '' ｝｝
 @endif
                 ｛｛ ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }} ? ${{ $variableName }}->{{ $belongsToRelations[$column['name']]['name'] }}->present()->toString() : '' ｝｝
 @else
@@ -70,8 +70,17 @@
         </div>
         <div class="box-body">
             <table class="table">
+                <th style="width: 10px">ID</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
 ＠foreach( ${{ $variableName }}->{{ array_get($relation, 'name') }}  as $relationModel )
-
+<tr>
+    <td>｛｛ $relationModel->id ｝｝</td>
+    <td>｛｛ $relationModel->present()->toString() ｝｝</td>
+    <td>
+        <a href="｛!! action('Admin\{{ ucfirst(\ICanBoogie\singularize(array_get($relation, 'name'))) }}Controller＠show', $relationModel->id) !!｝" class="btn btn-block btn-primary btn-sm"><i class="far fa-file-alt"></i> ＠lang('admin.pages.common.buttons.show')</a>
+    </td>
+</tr>
 ＠endforeach
             </table>
         </div>
