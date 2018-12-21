4a5829e590fea17e054a0df989966cd151d28741
ironhoang <ironhoang@gmail.com>
2018-12-21 13:40:58 +0700

    replate countByFilter instead of count

--- stubs/api/admin/controller.blade.php
+++ stubs/api/admin/controller.blade.php
@@ -54,7 +54,7 @@ class {{ $modelName }}Controller extends Controller
             $filter['query'] = $queryWord;
         }
 
-        $count      = $this->{{ $variableName }}Repository->count();
+        $count      = $this->{{ $variableName }}Repository->countByFilter();
         ${{ \ICanBoogie\pluralize($variableName) }} = $this->{{ $variableName }}Repository->getByFilter($filter, $order, $direction, $offset, $limit);
 
         return {{ \ICanBoogie\pluralize($modelName) }}::updateListWithModel(${{ \ICanBoogie\pluralize($variableName) }}, $offset, $limit, $count)->response();
